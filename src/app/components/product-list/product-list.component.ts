859db8f8a36b740b0a9f1136349840efe375da10
19jackM <jack.mokoena1234@gmail.com>
2025-01-21 15:32:55 +0200

    added working methods

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -6,20 +6,43 @@ import { Product } from '../../Models/product.models';
 
 
 @Component({
-
-selector: 'app-product-list'
+selector: 'app-product-list',
 standalone: true,
 imports: [CommonModule, RouterModule],
-templates: 
-
-
-
-
-
-
-
-
-
-
 
 })
+
+export class ProductListComponent implements OnInit{
+products: Product[] = [];
+    productsService: any;
+
+constructor(private productService: ProductService) {}
+
+ngOnInit(): void {
+    this.loadProducts();
+}
+
+loadProducts(): void {
+    this.productsService.getAll().subscribe({
+    next: (data) => {
+    this.products = data;
+},
+
+error: (e) => console.error(e),
+});
+}
+
+deleteProduct(id: number): void{
+    if(confirm('Are you sure you want delete this item?')){
+        this.productService.delete(id).subscribe({
+        next: () =>{
+            this.loadProducts();
+        },
+        
+        error: (e) =>console.error(e),
+
+
+    });
+}
+}
+}
