79451a7e0bb0b10582fc408a4de9552fa2c5df4a
19jackM <jack.mokoena1234@gmail.com>
2025-01-21 14:18:05 +0200

    added working Http methods

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,9 +13,27 @@ export class ProductService{
     private baseUrl = 'http://localhost:3000/api/products';
 
     constructor(private Http: HttpClient){ }
-
     
+    getAll(): Observable<Product[]>{
+        return this.Http.get<Product[]>(this.baseUrl);
+    }
 
+    get(id: number): Observable<Product>{
+        return this.Http.get<Product>(`${this.baseUrl}/${id}`);
+    }
 
+    create(data: Product): Observable<Product>{
 
+        return this.Http.post<Product>(this.baseUrl, data);
+    }
+
+    update(id: number, data: Product): Observable<Product>{
+
+        return this.Http.put<Product>(`${this.baseUrl}/${id}`, data);
+    }
+
+    delete(id: number): Observable<any>{
+
+        return this.Http.delete(`${this.baseUrl}/${id}`);
+    }
 }
