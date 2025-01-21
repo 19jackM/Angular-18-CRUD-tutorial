bbb1f625d74f1494a486ed893cb2fc8bbc3ab8c6
19jackM <jack.mokoena1234@gmail.com>
2025-01-21 12:55:12 +0200

    added injecatables

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,1 +1,25 @@
+import { Component, isStandalone, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../Models/product.models';
 
+
+@Component({
+
+selector: 'app-product-list'
+standalone: true,
+imports: [CommonModule, RouterModule],
+templates: 
+
+
+
+
+
+
+
+
+
+
+
+})
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,35 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../Models/product.models';
 
-Injectable({
+@Injectable({
 
 providedIn: 'root'
 })
 
 export class ProductService{
 
-private baseUrl = 'http://localhost:3000/api/products';
+    private baseUrl = 'http://localhost:3000/api/products';
 
-constructor(private http: HttpClient) { }
+    constructor(private Http: HttpClient){ }
 
-getAll(): Observable<Product>[]{
-return this.http.get<Product[]>(this.baseUrl)
-}
+    
 
-get(): Observable<Product> {
-return this.http.get<Product>(`${this.baseUrl}/${id});
-}
 
-create(data: product): Observable<Product> {
-return this.http.post<Product>(this.baseUrl, data);
-}
-
-update(id: number: Product): Observable<Product>{
-return this.http.put<Product>(`${this.baseUrl}/${id}`, data);
-}
-
-delete(id: number:): Observable<any> {
-return this.http.delete(`${this.baseUrl}/${id}`, data);
-}
 
 }
